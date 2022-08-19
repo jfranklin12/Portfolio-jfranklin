98fa91c514b11dfe0f5bcae450f25962b6cda1e2
jfranklin12 <j.franks725@gmail.com>
2022-08-19 15:13:37 -0400

    carousel added to mywork

---
+++ src/components/myWorkCarousel.js
@@ -0,0 +1,58 @@
+import '../App.css';
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import mistyLakeForest from '../images/mistyLakeForest.jpg';
+import riverInMountains from '../images/riverInMountains.jpg';
+import waterfallInCove from '../images/waterfallInCove.jpg';
+
+function MyWorkCarousel() {
+    const [index, setIndex] = useState(0);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+  
+    return (
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={mistyLakeForest}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={riverInMountains}
+            alt="Second slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+        <img 
+          className="d-block w-100" 
+          src={waterfallInCove}
+          alt="My Work Picture 3"
+        />
+  
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
+  
+  export default MyWorkCarousel;
