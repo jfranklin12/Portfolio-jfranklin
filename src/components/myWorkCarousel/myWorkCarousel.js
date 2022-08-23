6e6fc755877691f533ce08b4c0a7b528e760068e
jfranklin12 <j.franks725@gmail.com>
2022-08-22 21:16:54 -0400

    carousel media query completed

--- src/components/myWorkCarousel/myWorkCarousel.js
+++ src/components/myWorkCarousel/myWorkCarousel.js
@@ -1,79 +1,92 @@
-import '../../App.css';
-import React, { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import degrees from '../../images/degrees.jpg';
-import riverInMountains from '../../images/riverInMountains.jpg';
-import waterfallInCove from '../../images/waterfallInCove.jpg';
+import "../../App.css";
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import degrees from "../../images/degrees.jpg";
+import riverInMountains from "../../images/riverInMountains.jpg";
+import waterfallInCove from "../../images/waterfallInCove.jpg";
+import { Github } from 'react-bootstrap-icons';
 
 function MyWorkCarousel() {
-    const [index, setIndex] = useState(0);
-  
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-    };
-    
-    const projects = [
-      {
-        image: degrees,
-        projectTitle: '360 Degrees',
-        description: 'Blog for University Degrees',
-        github: 'https://github.com/jfranklin12/360-Degrees'
+  const [index, setIndex] = useState(0);
 
-      },
-      {
-        image: riverInMountains,
-        projectTitle: 'Touch Grass',
-        description: 'National Park Finder ',
-        github: 'https://github.com/jfranklin12/touch-grass-app-national-park-finder'
-      },
-      {
-        image: waterfallInCove,
-        projectTitle: 'Team Generator',
-        description: 'Generate an HTML for your Work Team',
-        github: 'https://github.com/jfranklin12/Julian-Franklin-Team-Profile-Generator'
-      },
-      {
-        image: waterfallInCove,
-        projectTitle: 'Work Day Scheduler',
-        description: 'Calendar for Scheduling your Work Day',
-        github: 'https://github.com/jfranklin12/Julian-Franklin-Team-Profile-Generator'
-      },
-      {
-        image: waterfallInCove,
-        projectTitle: 'Connect, Learn, Teach',
-        description: 'A Tech Blog',
-        github: 'https://github.com/jfranklin12/JFranklin-Connect-Learn-Teach'
-      },
-      {
-        image: waterfallInCove,
-        projectTitle: 'Social Network API Routers',
-        description: 'Backend Routes for a Social Network Application',
-        github: 'https://github.com/jfranklin12/Social-Network-API'
-      },
-    ]
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
-    return (
-      <Carousel activeIndex={index} onSelect={handleSelect} id="carousel">
-        {projects.map(item => (
-                  <Carousel.Item>
-                  <img
-                    className="d-block w-100"
-                    src={item.image}
-                    alt={item.projectTitle}
-                  />
-                  <Carousel.Caption>
-                    <h3>{item.projectTitle}</h3>
-                    <h5>{item.description}</h5>
-                    <p>{item.techs}</p>
-                    <a href={item.github} target="_blank">{item.projectTitle} Github</a>
-                  </Carousel.Caption>
-                </Carousel.Item>
+  const projects = [
+    {
+      id: 1,
+      image: degrees,
+      projectTitle: "360 Degrees",
+      description: "Blog for University Degrees",
+      github: "https://github.com/jfranklin12/360-Degrees",
+      website: "https://threesixtydegrees.herokuapp.com/"
+    },
+    {
+      id: 2,
+      image: riverInMountains,
+      projectTitle: "Touch Grass",
+      description: "National Park Finder ",
+      github:
+        "https://github.com/jfranklin12/touch-grass-app-national-park-finder",
+        website: "https://ashley1thompson.github.io/touch-grass-app-national-park-finder/"
+    },
+    {
+      id: 3,
+      image: waterfallInCove,
+      projectTitle: "Team Generator",
+      description: "Generate an HTML for your Work Team",
+      github:
+        "https://github.com/jfranklin12/Julian-Franklin-Team-Profile-Generator",
+      website: "https://drive.google.com/file/d/1qH84G3UVlhsRqG_EC-cIV7gSHI87lK4B/view?usp=sharing"
+    },
+    {
+      id: 4,
+      image: waterfallInCove,
+      projectTitle: "Work Day Scheduler",
+      description: "Calendar for Scheduling your Work Day",
+      github:
+        "https://github.com/jfranklin12/Julian-Franklin-Team-Profile-Generator",
+      website: "https://jfranklin12.github.io/Julian-Franklin-Work-Day-Scheduler/"
+    },
+    {
+      id: 5,
+      image: waterfallInCove,
+      projectTitle: "Connect, Learn, Teach",
+      description: "A Tech Blog",
+      github: "https://github.com/jfranklin12/JFranklin-Connect-Learn-Teach",
+      website: "https://julian-franklin-tech-blog.herokuapp.com/"
+    },
+    {
+      id: 6,
+      image: waterfallInCove,
+      projectTitle: "Social Network API",
+      description: "Backend Routes for a Social Network Application",
+      github: "https://github.com/jfranklin12/Social-Network-API",
+      website: "https://drive.google.com/file/d/1-RSisbQtVtqO8lXukffN7hBzR-ZbXb8T/view"
+    },
+  ];
 
-        ))
-
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect} id="carousel">
+      {projects.map((item) => (
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={item.image}
+            alt={item.projectTitle}
+            id="carouselImage"
+          />
+          <Carousel.Caption>
+            <h3><a href={item.website} target="_blank"  id="projTitle">{item.projectTitle}</a> <a href={item.github} target="_blank" id="projGit">
+              <Github /></a></h3>
+            <h5 id="projDes">{item.description}</h5>
+            
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
 }
-      </Carousel>
-    );
-  }
-  
-  export default MyWorkCarousel;
+
+export default MyWorkCarousel;
