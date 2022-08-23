e139bf5f236c61781c815f865da074400aa1c0f5
jfranklin12 <j.franks725@gmail.com>
2022-08-23 10:02:56 -0400

    initial form layout created for contact

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -5,9 +5,25 @@ import React from 'react';
 function Contact() {
   return (
     <section className='contact'>
-        <div className='container text-center'>
+      <div className='container text-center'>
         <h1 className="display-3">Contact Me!</h1>
-        </div>
+        <form>
+          <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">Name</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Email</label>
+            <input type="password" className="form-control" id="exampleInputPassword1"></input>
+          </div>
+          <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Message</label>
+            <textarea clasNames="form-control" aria-label="With textarea"></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
 
     </section>
 
