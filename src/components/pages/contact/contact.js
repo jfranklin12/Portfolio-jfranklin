7dc3def539712e72cf37010f53b62d969732fcbe
jfranklin12 <j.franks725@gmail.com>
2022-08-23 15:18:40 -0400

    form styled for mobile in contact

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -4,28 +4,35 @@ import React from 'react';
 
 function Contact() {
   return (
-    <section className='contact'>
-      <div className='container text-center'>
+    <section className="contact">
+      <div className="container text-center">
         <h1 className="display-3">Contact Me!</h1>
-        <form>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">Name</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">Email</label>
-            <input type="password" className="form-control" id="exampleInputPassword1"></input>
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">Message</label>
-            <textarea clasNames="form-control" aria-label="With textarea"></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-      </div>
 
-    </section>
+        <form>
+          <h2>Let's connect!</h2>
+          <h3>Send me a message, and I'll reach out to you!</h3>
+            <div className='row justify-content-center'>
+            <div className='col-12 col-md-8'>
+              <div className="mb-3">
+                <label for="exampleInputEmail1" className="form-label">Name</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+              </div>
+              <div className="mb-3">
+                <label for="exampleInputPassword1" className="form-label">Email</label>
+                <input type="password" className="form-control" id="exampleInputPassword1"></input>
+              </div>
+              <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              </div>
+            </div>
+            </div>
+          <button type="submit" className="btn btn-primary">SUBMIT</button>
+        </form>
+      </div >
+      
+
+    </section >
 
   );
 }
