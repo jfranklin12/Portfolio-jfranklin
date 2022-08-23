aa5aac3d1779bfce36ba3efb4b22a2ab7c60c8f0
jfranklin12 <j.franks725@gmail.com>
2022-08-23 16:28:07 -0400

    function created to handle user input on form

--- src/components/pages/contact/contact.js
+++ src/components/pages/contact/contact.js
@@ -1,8 +1,47 @@
 import '../../../App.css';
 import './contact.css'
-import React from 'react';
+import React, { useState} from 'react';
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [feedbackMessage, setFeedbackMessage] = useState('');
+  
+  const handleInputChange = (event) => {
+      const { target } = event;
+      const inputType = target.name;
+      const inputValue = target.value;
+
+      if (inputType === 'name') {
+          setName(inputValue);
+      } else if (input === 'email') {
+          setEmail(inputValue);
+      } else {
+          setMessage(inputValue);
+      }
+  };
+
+  const handleFormSubmit = (event) => {
+      event.preventDefault();
+
+      if(!email || !name || !message) {
+          setErrorMessage('This field is required');
+          return;
+      }
+      if (!validateEmail(email)) {
+          setErrorMessage('Please enter a valid email');
+          return;
+      } else {
+        setFeedbackMessage(`Thank you ${name}! I'll be in touch soon!`)
+      }
+
+      setName('');
+      setEmail('');
+      setMessage('');
+  };
+
   return (
     <section className="contact">
       <div className="container text-center">
