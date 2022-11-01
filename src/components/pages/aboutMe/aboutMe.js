7db44936a7dc99a9843c9bc67da03e3db0836928
jfranklin12 <j.franks725@gmail.com>
2022-11-01 15:36:05 -0400

    updated about me and resume

--- src/components/pages/aboutMe/aboutMe.js
+++ src/components/pages/aboutMe/aboutMe.js
@@ -10,11 +10,7 @@ function AboutMe() {
         <h1 className="display-3">About Me</h1>
             <img src={myHeadshot} alt="My Picture" id="myHeadshot"></img>
         <p className="lead">
-          I am thrilled to announce my long-awaited career transformation into web development has begun! My name is Julian Franklin. For the last seven years, I have been a top-notch, highly recognized, elementary school educator. Now, as I commence this new chapter, it is important to hold tight to the joy and success I have experienced in education. By leveraging my aptitude for creativity, organization, planning, agility, and perfection, I know I will continue to produce exceptional products just like I did in education. I am excited to learn and apply the skills Georgia Tech will develop in me as a Software Engineer.
-          <br></br><br></br>
-          It is my drive for learning and growing that has inspired this new adventure. And what an adventure it has been already! Every day I become more fluent in HTML development, using CSS and JavaScript, getting to put my creativity to the test. There is no doubt over the next few months, Georgia Tech’s program, combined with my persistence, will forge me into a talented software engineer.
-          <br></br><br></br>
-          I am looking forward to contributing my unique skills to a team of like-minded, passionate professionals!
+        Mission-oriented full stack web developer leveraging education background to build a functional and seamless developer to user experience. Recently earned a full stack web development certificate from Georgia Institute of Technology excelling in newly developed skills in JavaScript, CSS, React.js and responsive web design. Known as an revolutionary problem solver passionate about developing detail-oriented, innovative apps that focus on mobile-first design and development. In projects, my goal is to create an app that excites users through its state of the art technology and flawless experience. I worked on a team of four to develop a single-page MERN app that allows individuals to connect, place and accept bids for contract web development jobs. I was able to implement my newly developed database skills in MongoDB to create a database that functioned through React and Heroku. I’m thrilled to exceed expectations by working in an agile environment driven by collaboration, efficiency and determination to build extraordinary web experiences.
           </p>
       </div>
     </section>
--- src/components/pages/resume/resume.js
+++ src/components/pages/resume/resume.js
@@ -1,9 +1,17 @@
 import '../../../App.css';
 import "./resume.css"
+
 import React from 'react';
 
 function Resume() {
 
+  // const resume = () => {
+  //   fetch('Julian-Franklin-Resume-2022.pdf').then(response => {
+  //     response.blob().then(blob => {
+  //       const fileURL
+  //     })
+  //   })
+  // }
 
     return (
       <div className="resume">
@@ -11,10 +19,10 @@ function Resume() {
         <h1 className="display-3">Resume</h1>
         <div className='row justify-content-center'>
           <div className="col-6 col-sm-5 col-lg-4 col-xl-3">
-        <button type="button" className="btn btn-secondary" id="resume-button"><a href="https://docs.google.com/document/d/e/2PACX-1vTHePs_3_LaGuFBkxwo2u_uDv2u81yyaOASFc0oTf7jAPAwbM-354EHD1h4TAy01w/pub" target="_blank" id="link">Download</a></button>
+        <button type="button" className="btn btn-secondary" id="resume-button"><a href={require("./Julian-Franklin-Resume-2022.pdf")} target="_blank" id="link">Download</a></button>
         </div>
         <div className="col-6 col-sm-5 col-lg-4 col-xl-3">
-        <button type="button" className="btn btn-secondary" id="resume-button"><a href="https://docs.google.com/document/d/e/2PACX-1vTHePs_3_LaGuFBkxwo2u_uDv2u81yyaOASFc0oTf7jAPAwbM-354EHD1h4TAy01w/pub" target="_blank" id="link">View Online</a></button>
+        <button type="button" className="btn btn-secondary" id="resume-button"><a href="https://docs.google.com/document/d/e/2PACX-1vRAdVBQb3NFSHbAfO44nRmUjiaCxp3HrJeoWRK96LFjpwL-zfSltaIEHXeQw88esA8PDX8JRI41OISM/pub" target="_blank" id="link">View Online</a></button>
         </div>
         </div>  
       </div>
